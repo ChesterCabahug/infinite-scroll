dc0a659e2514bded3cd84552b6f4d0d76aa71f50
Chester <chesterdeveloper2000@gmail.com>
2020-07-22 19:29:59 +0800

    finally fetched the photos and placing it in the DOM!

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+const imageContainer = document.getElementById("image-container")
+const loader = document.getElementById("loader")
+
+
+let photosArray = []
+
+
 // unsplash api
 const count = 10
 const apiKey = "SCLtf3C-D4Vu-_r1F0D_gSCUWYEL8-fjv2kcbX04XMo"
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+
+
+
+// create elements for links and photos add to dom
+
+
+displayPhotos = () => {
+    photosArray.forEach((photo) => {
+        console.log(photo)
+        // create <a></a>  to link to unsplash
+        const item = document.createElement("a")
+        item.setAttribute("href", photo.links.html)
+        item.setAttribute("target", "_blank")
+
+
+        // create <img> for photo
+        const img = document.createElement("img")
+        img.setAttribute("src", photo.urls.regular)
+        img.setAttribute("alt", photo.alt_description)
+        img.setAttribute("title", photo.alt_description)
+
+
+        // put <img> inside <a></a>.. then put inside imageContainer
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+    })
+}
 
 
 // get photos from unsplash
@@ -9,8 +44,8 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 getPhotos = async() => {
     try {
         const response = await fetch(apiUrl)
-        const data = await response.json()
-        console.log(data)
+        photosArray = await response.json()
+        displayPhotos()
     } catch (error) {
         
     }
